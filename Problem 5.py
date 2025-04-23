68230dd11b8771033df6e28aaa64db7e694d0cb0
ayushpatel7139 <ayushpatel7186@gmail.com>
2025-04-23 13:02:55 +0530

    Problem 5.py

--- Problem 5.py
+++ Problem 5.py
@@ -1,5 +1,3 @@
-# Write a program to copy contents of one file to another. While doing so, replace all lowercase characters into uppercase characters.
-
 with open("Q5.txt", "r") as source_file:
     content = source_file.read()
     upper_content = content.upper()
