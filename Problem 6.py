83cbcf71d42ff7a6006adbbb0415e30b2c71bd0d
ayushpatel7139 <ayushpatel7186@gmail.com>
2025-04-23 13:03:15 +0530

    Problem 6.py

--- Problem 6.py
+++ Problem 6.py
@@ -1,6 +1,3 @@
-# Write a program that merges lines alternatively from two files and writes the results to new file. If one file has less number of lines than the other,  the remaining lines from the larger file should be simply copied into the target file.
-
-
 with open("Q6_file1.txt", "r") as file1, open("Q6_file2.txt", "r") as file2, open("Q6_merged.txt", "w") as merged_file:
     lines1 = file1.readlines()
     lines2 = file2.readlines()
