95295cc42b3db2295d0e6b0587500fa32897f869
ayushpatel7139 <ayushpatel7186@gmail.com>
2025-04-23 13:01:42 +0530

    Problem 2.py

--- Problem 2.py
+++ Problem 2.py
@@ -1,10 +1,7 @@
-# Read the data stored in MS-Excel file and convert it into a dictionary. The record contains rollno, name of student, marks of three subjects. Also calculate total. Display the dictionary data on the monitor.
-
 import csv
 
 with open("Q2.csv", "r") as f:
     reader = csv.DictReader(f)
-    # Create a list of dictionaries
     data = []
     for row in reader:
         # Convert marks to integers and calculate total
@@ -13,6 +10,5 @@ with open("Q2.csv", "r") as f:
         row['Chemistry'] = int(row['Chemistry'])
         row['total'] = row['CPII'] + row['MathsII'] + row['Chemistry']
         data.append(row)
-    # Print the list of dictionaries
     for record in data:
         print(record)
